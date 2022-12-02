f463d79fad3f06176cab0534e2894f03375d2407
DougEke <95352026+DougEke@users.noreply.github.com>
2022-12-02 12:40:45 +0000

    first commit

---
+++ src/main/java/weatherservice/weatherservicebackend/WeatherServiceBackEndApplication.java
@@ -0,0 +1,13 @@
+package weatherservice.weatherservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServiceBackEndApplication.class, args);
+	}
+
+}
